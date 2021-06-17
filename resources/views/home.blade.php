38f181f1515ffd5e10031eb309d10e11d918ccc6
Ciarm-nik <nikcia@libero.it>
2021-06-17 16:39:02 +0200

    Esercizio completo

---
+++ resources/views/home.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="it">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel: primi passi</title>
+</head>
+
+<body>
+    <h1>Hello Laravel</h1>
+
+
+    <form action="">
+        <input type="text" value=" {{$name}} ">
+        <input type="text" value=" {{$surname}} ">
+        <input type="text" value=" {{$email}} ">
+    </form>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home', [
+        "name" => "Nicola",
+        "surname" => "Ciarmoli",
+        "email" => "ciarmolinicola@libero.it"
+    ]);
+})->name("home");
